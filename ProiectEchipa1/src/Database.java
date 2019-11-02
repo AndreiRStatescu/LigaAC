28c2ac6548e787a8c3458129918384a8e8e3ad08
liviu0703 <55464964+liviu0703@users.noreply.github.com>
2019-11-02 18:07:56 +0200

    Delete

--- ProiectEchipa1/src/Database.java
+++
@@ -1,9 +0,0 @@
-package pack;
-
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Map;
-
-public class Database {
-	public static ArrayList<User> users=new ArrayList<User>();
-}
